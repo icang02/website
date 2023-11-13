8448a30a5ea2a018760d28446060be1a0708eb6b
icang02 <ilmifaiza1112@gmail.com>
2023-11-14 05:19:18 +0800

    uppp

--- app/page.jsx
+++ app/page.jsx
@@ -5,13 +5,15 @@ import Text from "@/components/Home/Text";
 import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
+  const APP_URL = process.env.NEXTAUTH_URL;
+
   return (
     <>
       <Navbar />
       <Hero />
-      <CardCourse />
+      <CardCourse APP_URL={APP_URL} />
       <Text />
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- components/Home/CardCourse.jsx
+++ components/Home/CardCourse.jsx
@@ -1,29 +1,28 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { AiOutlinePlayCircle } from "react-icons/ai";
 import { FaArrowRightLong } from "react-icons/fa6";
 
-
-export default function CardCourse() {
+export default function CardCourse({ APP_URL }) {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getMateri() {
       try {
-        const res = await fetch(`http://localhost:3000/api/courses`)
-        const resJson = await res.json()
-        setData(resJson)
+        const res = await fetch(`${APP_URL}/api/courses`);
+        const resJson = await res.json();
+        setData(resJson);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
     }
 
-    getMateri()
-  }, [])
+    getMateri();
+  }, []);
 
   return (
     <div className="pt-10 pb-16 px-4">
@@ -31,33 +30,47 @@ export default function CardCourse() {
         Let&apos;s <span className="text-blue-500"> Code</span>
       </h1>
       <p className="text-p text-sm mb-7 text-center leading-6">
-        Daftar materi yang telah <span className="font-medium text-blue-500"> dirilis</span>
+        Daftar materi yang telah{" "}
+        <span className="font-medium text-blue-500"> dirilis</span>
       </p>
 
       <div className="grid grid-cols-12 gap-y-10 md:gap-12">
         {loading ? (
-          <div className="col-span-12 text-sm text-center">
-            Loading data...
-          </div>
+          <div className="col-span-12 text-sm text-center">Loading data...</div>
         ) : (
           data.map((item, i) => (
             <div className="col-span-12 md:col-span-4" key={i}>
-              <a href={"/belajar/" + item.slug} className="group block w-full aspect-video overflow-hidden rounded-xl">
-                <div className="w-full h-full bg-center bg-cover group-hover:scale-110 transition-all ease-out duration-500" style={{ backgroundImage: `url(${item.image})` }}></div>
+              <a
+                href={"/belajar/" + item.slug}
+                className="group block w-full aspect-video overflow-hidden rounded-xl"
+              >
+                <div
+                  className="w-full h-full bg-center bg-cover group-hover:scale-110 transition-all ease-out duration-500"
+                  style={{ backgroundImage: `url(${item.image})` }}
+                ></div>
               </a>
 
               <div className="leading-6">
                 <div className="border-l-2 border-r-2 rounded border-gray-50">
                   <div className="px-3">
-                    <h6 className="block font-bold mt-5 text-sm text-gray-800">{item.title}</h6>
-                    <p className="text-p mt-3 text-xs leading-5">{item.description}</p>
+                    <h6 className="block font-bold mt-5 text-sm text-gray-800">
+                      {item.title}
+                    </h6>
+                    <p className="text-p mt-3 text-xs leading-5">
+                      {item.description}
+                    </p>
                   </div>
                 </div>
 
                 <div className="px-1 flex items-center justify-between mt-5">
-                  <div className="border border-blue-500 text-blue-500 rounded px-3 py-1 text-xs font-semibold">2 Bagian</div>
+                  <div className="border border-blue-500 text-blue-500 rounded px-3 py-1 text-xs font-semibold">
+                    2 Bagian
+                  </div>
 
-                  <Link href={`belajar/${item.slug}`} className="flex items-center gap-1 text-gray-800 text-sm font-semibold hover:text-blue-500">
+                  <Link
+                    href={`belajar/${item.slug}`}
+                    className="flex items-center gap-1 text-gray-800 text-sm font-semibold hover:text-blue-500"
+                  >
                     <span>Mulai Belajar</span>
                     <AiOutlinePlayCircle className="text-xl" />
                   </Link>
@@ -80,5 +93,5 @@ export default function CardCourse() {
         </div>
       )}
     </div>
-  )
+  );
 }
