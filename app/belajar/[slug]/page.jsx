dec515d6f52b93b78fe6fad12677aea5baef4f8b
icang02 <ilmifaizan1112@gmail.com>
2023-11-17 07:31:36 +0800

    add

--- app/belajar/[slug]/page.jsx
+++ app/belajar/[slug]/page.jsx
@@ -3,14 +3,17 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 
 export const metadata = {
-  title: process.env.APP_NAME +  " | Course",
+  title: process.env.APP_NAME + " | Course",
 };
 
-export default function CourseDetail() {
+export default async function Page({ params: { slug } }) {
+  const res = await fetch(`${process.env.APP_URL}/api/courses/${slug}`, { cache: "no-store" });
+  const courses = await res.json();
+
   return (
     <>
       <Navbar />
-      <Detail />
+      <Detail courses={courses} />
       <Footer />
     </>
   );
--- components/Home/CardCourse.jsx
+++ components/Home/CardCourse.jsx
@@ -1,28 +1,10 @@
-"use client";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import { AiOutlinePlayCircle } from "react-icons/ai";
 import { FaArrowRightLong } from "react-icons/fa6";
 
-export default function CardCourse() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function getMateri() {
-      try {
-        const res = await fetch(`${process.env.APP_URL}/api/courses`);
-        const courses = await res.json();
-        setData(courses);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    getMateri();
-  }, []);
+export default async function CardCourse() {
+  const res = await fetch(`${process.env.APP_URL}/api/courses`, { cache: "no-store" });
+  const data = await res.json();
 
   return (
     <div className="pt-10 pb-16 px-4">
@@ -30,62 +12,41 @@ export default function CardCourse() {
         Let&apos;s <span className="text-blue-500"> Code</span>
       </h1>
       <p className="text-p text-sm mb-7 text-center leading-6">
-        Daftar materi yang telah{" "}
-        <span className="font-medium text-blue-500"> dirilis</span>
+        Daftar materi yang telah <span className="font-medium text-blue-500"> dirilis</span>
       </p>
 
       <div className="grid grid-cols-12 gap-y-10 md:gap-12">
-        {loading ? (
-          <div className="col-span-12 text-sm text-center">Loading data...</div>
-        ) : data.length == 0 ? (
-          <div className="col-span-12 text-sm text-center">Belum ada course tersedia.</div>
-        ) : (
-          data.map((item, i) => (
+        {data.map(
+          (item, i) =>
             item.course_part.length > 0 && (
               <div className="col-span-12 md:col-span-4" key={i}>
-              <Link
-                href={`/belajar/${item.slug}`}
-                className="group block w-full aspect-video overflow-hidden rounded-xl"
-              >
-                <div
-                  className="w-full h-full bg-center bg-cover group-hover:scale-105 transition-all ease-out duration-500"
-                  style={{ backgroundImage: `url(${item.image})` }}
-                ></div>
-              </Link>
+                <Link href={`/belajar/${item.slug}`} className="group block w-full aspect-video overflow-hidden rounded-xl">
+                  <div className="w-full h-full bg-center bg-cover group-hover:scale-105 transition-all ease-out duration-500" style={{ backgroundImage: `url(${item.image})` }}></div>
+                </Link>
 
-              <div className="leading-6">
-                <div className="border-l-2 border-r-2 rounded border-gray-50">
-                  <div className="px-3">
-                    <h6 className="block font-bold mt-5 text-sm text-gray-800">
-                      {item.title}
-                    </h6>
-                    <p className="text-p mt-3 text-xs leading-5">
-                      {item.description}
-                    </p>
-                  </div>
-                </div>
-
-                <div className="px-1 flex items-center justify-between mt-5">
-                  <div className="border border-blue-500 text-blue-500 rounded px-3 py-1 text-xs font-semibold">
-                    {item.course_part.length} Bagian
+                <div className="leading-6">
+                  <div className="border-l-2 border-r-2 rounded border-gray-50">
+                    <div className="px-3">
+                      <h6 className="block font-bold mt-5 text-sm text-gray-800">{item.title}</h6>
+                      <p className="text-p mt-3 text-xs leading-5">{item.description}</p>
+                    </div>
                   </div>
 
-                  <Link
-                    href={`/belajar/${item.slug}`}
-                    className="flex items-center gap-1 text-gray-800 text-sm font-semibold hover:text-blue-500"
-                  >
-                    <span>Mulai Belajar</span>
-                    <AiOutlinePlayCircle className="text-xl" />
-                  </Link>
+                  <div className="px-1 flex items-center justify-between mt-5">
+                    <div className="border border-blue-500 text-blue-500 rounded px-3 py-1 text-xs font-semibold">{item.course_part.length} Bagian</div>
+
+                    <Link href={`/belajar/${item.slug}`} className="flex items-center gap-1 text-gray-800 text-sm font-semibold hover:text-blue-500">
+                      <span>Mulai Belajar</span>
+                      <AiOutlinePlayCircle className="text-xl" />
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
             )
-          ))
         )}
       </div>
 
-      {!loading && data.length != 0 && (
+      {data.length != 0 && (
         <div className="mt-5 flex justify-end">
           <a
             href={"/"}
