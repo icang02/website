074ab32575fec3e67e7322ff56675b93b2c6af73
icang02 <ilmifaizan1112@gmail.com>
2023-11-17 11:07:38 +0800

    dsfsdfs

--- app/api/courses/id/[id]/create/route.js
+++ app/api/courses/id/[id]/create/route.js
@@ -2,32 +2,40 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 export async function POST(request) {
   const reqBody = await request.json();
   const { courses_id, order, title, content } = reqBody;
 
   // add class style in content
-  const x1 = content.replaceAll('<h4>', '<h4 class="h4">');
-  const x2 = x1.replaceAll('<p>', '<p class="p">');
-  const x3 = x2.replaceAll('<ul>', '<ul class="ul">');
-  const x4 = x3.replaceAll('<ol>', '<ol class="ol">');
-  const x5 = x4.replaceAll('<li>', '<li class="li">');
-  const x6 = x5.replaceAll('<img>', '<img class="img">');
+  // const x1 = content.replaceAll('<h4>', '<h4 class="h4">');
+  // const x2 = x1.replaceAll('<p>', '<p class="p">');
+  // const x3 = x2.replaceAll('<ul>', '<ul class="ul">');
+  // const x4 = x3.replaceAll('<ol>', '<ol class="ol">');
+  // const x5 = x4.replaceAll('<li>', '<li class="li">');
+  // const x6 = x5.replaceAll('<img>', '<img class="img">');
+  const contentAddClass = content
+    .replaceAll("<h1>", '<h1 class="h1">')
+    .replaceAll("<h2>", '<h2 class="h2">')
+    .replaceAll("<h3>", '<h3 class="h3">')
+    .replaceAll("<h4>", '<h4 class="h4">')
+    .replaceAll("<p>", '<p class="p">')
+    .replaceAll("<ul>", '<ul class="ul">')
+    .replaceAll("<ol>", '<ol class="ol">')
+    .replaceAll("<li>", '<li class="li">')
+    .replaceAll("<img>", '<img class="img">');
 
-
-  console.log(x5);
+  console.log(contentAddClass);
 
   const course_part = await prisma.course_part.create({
     data: {
       courses_id: parseInt(courses_id),
       order: order,
       title: title,
-      content: x5,
+      content: contentAddClass,
     },
   });
   await prisma.$disconnect();
-  
 
-  return NextResponse.json({ 'course_part': 'sds' });
+  return NextResponse.json({ course_part: "sds" });
 }
